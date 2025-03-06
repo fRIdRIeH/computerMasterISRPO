6983ed77d561677018271fd767a9fdae67b4db22
fRIdRIeH <wladwaliullin@yandex.ru>
2025-03-06 19:00:17 +0400

    added mysqldata

--- obj/Debug/net8.0-windows/ComputerMaster.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ComputerMaster.AssemblyInfo.cs
@@ -14,7 +14,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComputerMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b90468d9c27bff7d29da1cbbb4eb6eda48e22fc6")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("ComputerMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComputerMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
